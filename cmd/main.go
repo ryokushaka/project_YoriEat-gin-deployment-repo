91b38834cbd5512f7b5b8445f423d3124f3a53c0
devYuMinKim <gimyumin40@gmail.com>
2024-08-02 12:06:05 +0900

    📝 docs: Add OpenAPI specification and Swagger documentation
    Added OpenAPI specification to document API endpoints, parameters, and responses. Created `openapi.yaml` file with necessary schema and endpoint information. Integrated Swagger to visualize the API and provide an automated testing interface.
    
    - Created `cmd/docs/openapi.yaml` with API schema and endpoints
    - Generated `cmd/docs/swagger.json` and `cmd/docs/swagger.yaml`
    - Added Swagger integration in `cmd/docs/docs.go`
    
    Related to #25

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/ryokushaka/project_YoriEat-gin-deployment-repo/api/middleware"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ryokushaka/project_YoriEat-gin-deployment-repo/api/route"
 	"github.com/ryokushaka/project_YoriEat-gin-deployment-repo/bootstrap"
+	_ "github.com/ryokushaka/project_YoriEat-gin-deployment-repo/cmd/docs"
 )
 
 func main() {
@@ -27,6 +31,16 @@ func main() {
 	timeout := time.Duration(env.ContextTimeout) * time.Second
 
 	router := gin.Default()
+
+	router.Use(gin.Logger())      // 요청에 대한 로깅을 수행
+	router.Use(gin.Recovery())    // Panic이 발생했을 때 복구하고 500 에러를 반환
+	router.Use(middleware.CORS()) // CORS 미들웨어 추가
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "UP"})
+	})
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	route.Setup(env, timeout, app.Postgres.DB, router)
 
