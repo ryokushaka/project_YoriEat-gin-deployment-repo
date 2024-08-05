73eb62d80c20c57880a65ff08860ac545bd22c07
devYuMinKim <gimyumin40@gmail.com>
2024-08-05 22:23:06 +0900

    ✨ feat: add CORS and LiteAuth middleware
    - Implement CORS middleware in cors_middleware.go
    - Implement LiteAuth middleware in lite_auth_middleware.go
    - Integrate middleware into main.go for handling requests
    
    Close #31

---
+++ api/middleware/cors_middleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// CORS 미들웨어 설정
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
